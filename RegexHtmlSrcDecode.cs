3aa3b5c1e87221919240956a1a51098512f54a08
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2021-01-01 23:28:55 +0300

    Update and rename RegexHtmlSrcDecode to RegexHtmlSrcDecode.cs

--- RegexHtmlSrcDecode.cs
+++ RegexHtmlSrcDecode.cs
@@ -1,4 +1,11 @@
-        public string  regexsrc(string EBSHtmlveri)
+namespace ebstimecms
+{
+    public class kategoriler
+    {
+       public string  regexsrc(string EBSHtmlveri)
         {
             return Regex.Match(EBSHtmlveri, "<img.+?src=[\"'](.+?)[\"'].*?>", RegexOptions.IgnoreCase).Groups[1].Value;
         }
+    }
+}
+
